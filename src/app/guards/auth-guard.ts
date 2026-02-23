6cb2b754c722517053a2aba83f77265b82bc4ed1
Dark Light <pazsebas@hotmail.com>
2026-02-23 15:15:22 -0500

    feat: implement auth guard for route protection and user authentication

--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -1,5 +1,22 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth';
+import { map, tap } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  return authService.isLoggedIn$.pipe(
+    // Si no está autenticado, redirigir al login
+    tap((isAuthenticated) => {
+      if (!isAuthenticated) {
+        console.log('🚫 Acceso denegado - Usuario no autenticado');
+        router.navigate(['/auth']);
+      } else {
+        console.log('✅ Acceso permitido - Usuario autenticado');
+      }
+    }),
+    // Retornar el estado de autenticación
+    map((isAuthenticated) => isAuthenticated),
+  );
 };
